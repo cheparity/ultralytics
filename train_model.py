10dcdffdc220afa997351805bbb027d869f1b15f
Niyuta <cheparty@gmail.com>
2025-03-21 16:50:27 +0800

    modify train_model.py using the relative path

--- train_model.py
+++ train_model.py
@@ -5,10 +5,10 @@ model = YOLO("yolo11n.pt")
 
 # 训练模型
 train_results = model.train(
-    data="/Users/liuyucheng/Projects/yolov11/ultralytics/cfg/datasets/tinyperson.yaml",  # 数据集 YAML 路径
+    data="./ultralytics/cfg/datasets/tinyperson.yaml",  # 数据集 YAML 路径
     epochs=100,  # 训练轮次
     imgsz=640,  # 训练图像尺寸
-    device=0,  # 运行设备，例如 device=0 或 device=0,1,2,3 或 device=cpu
+    device='mps',  # 运行设备，例如 device=0 或 device=0,1,2,3 或 device=cpu
 )
 
 # 评估模型在验证集上的性能
@@ -16,7 +16,7 @@ metrics = model.val()
 
 # 在图像上执行对象检测
 results = model(
-    "/Users/liuyucheng/Projects/yolov11/ultralytics/cfg/datasets/tinyperson/test/images/baidu_P000_4_jpg.rf.b5255d9f0b569e85fab6d0998a6dbfb0.jpg"
+    "./ultralytics/cfg/datasets/tinyperson/test/images/baidu_P000_4_jpg.rf.b5255d9f0b569e85fab6d0998a6dbfb0.jpg"
 )
 results[0].show()
 
